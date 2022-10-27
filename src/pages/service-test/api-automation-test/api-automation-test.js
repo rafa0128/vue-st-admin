4fe3f865c0268de3a4f8a905b23b501710a04deb
Rafa Chen <laoqi1988_f1@126.com>
2022-10-27 15:00:45 +0800

    接口自动化测试界面设计

--- src/pages/service-test/api-automation-test/api-automation-test.js
+++ src/pages/service-test/api-automation-test/api-automation-test.js
@@ -1,6 +1,40 @@
 /* eslint-disable */
 import { defineComponent } from 'vue';
+import { Search, Plus } from '@element-plus/icons-vue';
 export default defineComponent({
-    name: 'ApiAutomationTest'
+    name: 'ApiAutomationTest',
+    setup: function () {
+        var tableData = [
+            {
+                project: '项目A',
+                task: '正式环境接口监控',
+                status: '成功',
+                time_rule: '5min执行',
+                utime: '2022:10:20',
+                creater: 'Rafa Chen',
+            },
+            {
+                project: '项目A',
+                task: '正式环境接口监控',
+                status: '成功',
+                time_rule: '5min执行',
+                utime: '2022:10:20',
+                creater: 'Rafa Chen',
+            },
+            {
+                project: '项目A',
+                task: '正式环境接口监控',
+                status: '成功',
+                time_rule: '5min执行',
+                utime: '2022:10:20',
+                creater: 'Rafa Chen',
+            }
+        ];
+        return {
+            tableData: tableData,
+            Search: Search,
+            Plus: Plus
+        };
+    }
 });
 //# sourceMappingURL=api-automation-test.js.map
--- src/pages/service-test/api-automation-test/api-automation-test.ts
+++ src/pages/service-test/api-automation-test/api-automation-test.ts
@@ -1,6 +1,41 @@
 /* eslint-disable */
 import {defineComponent, onMounted, onUnmounted} from 'vue';
-
+import { Delete, Edit, Search, Plus, Upload } from '@element-plus/icons-vue'
 export default defineComponent({
-    name: 'ApiAutomationTest'
+    name: 'ApiAutomationTest',
+    setup(){
+        const tableData = [
+            {
+                project: '项目A',
+                task: '正式环境接口监控',
+                status: '成功',
+                time_rule: '5min执行',
+                utime: '2022:10:20',
+                creater: 'Rafa Chen',
+            },
+            {
+                project: '项目A',
+                task: '正式环境接口监控',
+                status: '成功',
+                time_rule: '5min执行',
+                utime: '2022:10:20',
+                creater: 'Rafa Chen',
+            },
+            {
+                project: '项目A',
+                task: '正式环境接口监控',
+                status: '成功',
+                time_rule: '5min执行',
+                utime: '2022:10:20',
+                creater: 'Rafa Chen',
+            }
+
+        ];
+
+        return{
+            tableData,
+            Search,
+            Plus
+        }
+    }
 });
